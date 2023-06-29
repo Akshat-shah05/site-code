b3589cf3e18d6270c766dea14fd5fb92a7b1d00d
Akshat-shah05 <akshatshah1605@gmail.com>
2023-06-29 01:42:20 -0400

    Main Home page complete!

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,21 +5,12 @@ const Footer = () => {
     return (
         <>
             <div className="footer-container">
-                <footer className="footer">
-                    <div className="contact-info">
-                        <p>
-                            Email: Akshatshah1605@gmail.com
-                        </p>
-                        <p>
-                            Set a Meeting: --Calendly Link--
-                        </p>
-                    </div>
-                    <div className="social-links">
-                        <img></img> {/* Instagram Link */}
-                        <img></img> {/* Github Link */}
-                        <img></img> {/* Twitter Link */}
-                    </div>
-                </footer>
+                <p> Email: Akshatshah1605@gmail.com </p>
+                <ul className="footer-list"> 
+                    <li> Instagram </li>
+                    <li> Github </li>
+                    <li> Twitter </li>
+                </ul>
             </div>
         </>
     )
