54d6b050b61fbb53fbe88273cb1ec0ba88b80be0
Akshat-shah05 <akshatshah1605@gmail.com>
2023-07-04 01:33:00 -0400

    background + structure/logic changes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import './App.css';
 import Navbar from './components/Navbar'
 import Intro from './components/Intro'
 import Footer from './components/Footer'
+import About from './pages/About/aboutMe'
+import Projects from './pages/Projects/Projects'
 
 function App() {
   return (
-    <div className='main-body'>
-      <Navbar/>
-      <Intro/>
-      <Footer/>
-    </div>
+     <>
+      <div className='main-body'>
+        <Navbar/>
+        <Intro/>
+        <About />
+        <Projects />
+        <Footer/>
+      </div>
+     </> 
+      
   );
 }
 
