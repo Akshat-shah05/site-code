8a80f0d3cd1983d4197e97e1d1e45814bf468ba9
akshat-shah05 <akshatshah1605@gmail.com>
2024-01-21 16:14:35 -0500

    experience constants

---
+++ src/pages/Experience/constants.js
@@ -0,0 +1,36 @@
+import watstreet from "../../assets/WATSTREET.jpg"
+
+const experiences = [
+    {
+      title: "Full Stack Developer",
+      company_name: "Wat Street",
+      icon: watstreet,
+      iconBg: "#383E56",
+      date: "Sept 2023 - Jan 2024",
+      points: [
+        "Responsible for the website and algorithmic trading dashboard - Reduced render time by 12% as measured by React Dev Tools and Lighthouse"
+      ],
+    },
+    {
+      title: "Freelance Frontend Web Developer",
+      company_name: "Fiver",
+      icon: watstreet,
+      iconBg: "#E6DEDD",
+      date: "Feb 2023 - Aug 2023",
+      points: [
+        "Produced websites and web apps for clients on Fiverr - Achieved 4.7/5.0 star rating"
+      ],
+    },
+    {
+      title: "Mathematics Tutor",
+      company_name: "Math2Success",
+      icon: watstreet,
+      iconBg: "#383E56",
+      date: "May 2022 - Jul 2023",
+      points: [
+        "Taught mathematics to students from grades 11 and 12 - Received immensly position reviews with a 96% satisfaction rate",
+      ],
+    },
+  ];
+
+  export { experiences }
